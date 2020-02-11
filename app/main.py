b400a0bbdc6b4d662ec90500e3c9843433620723
Ryan Levi <ryan.e.levi@gmail.com>
2020-02-10 20:23:05 -0800

    init commit
    :#

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, Blueprint, request, Response, render_template, flash, redirect
+from forms import LoginForm
+
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+# Index
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
